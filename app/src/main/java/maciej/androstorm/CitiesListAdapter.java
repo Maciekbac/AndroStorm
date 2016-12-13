4a4b99ad0e1989991b6a5d4a7730271670971bbe
Maciej <maciekbac@gmail.com>
2016-12-13 19:43:15 +0100

    Dodano oznaczanie ulubionych miast

--- app/src/main/java/maciej/androstorm/CitiesListAdapter.java
+++ app/src/main/java/maciej/androstorm/CitiesListAdapter.java
@@ -2,16 +2,20 @@ package maciej.androstorm;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.CursorAdapter;
 import android.widget.TextView;
-
+import android.widget.Toast;
 
 
 public class CitiesListAdapter extends CursorAdapter {
 
+    CheckBox cb;
 
     public CitiesListAdapter(Context context, Cursor c, int flags) {
         super(context, c, flags);
@@ -23,9 +27,25 @@ public class CitiesListAdapter extends CursorAdapter {
     }
 
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
+    public void bindView(View view, final Context context, Cursor cursor) {
         TextView tv = (TextView)view.findViewById(R.id.cli_city);
+        cb = (CheckBox)view.findViewById(R.id.cli_cBox);
+
         String city = cursor.getString(cursor.getColumnIndexOrThrow("city"));
         tv.setText(city);
+        cb.setChecked(cursor.getInt(cursor.getColumnIndexOrThrow("fav"))!=0);
+
+        final int cityId = cursor.getInt(cursor.getColumnIndex("_id"));
+        cb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
+                SQLiteDatabase db = new CitiesDatabase(context).getWritableDatabase();
+                if (checked)
+                    db.execSQL("UPDATE Cities SET fav = 1 WHERE _id = "+cityId);
+                else
+                    db.execSQL("UPDATE Cities SET fav = 0 WHERE _id = "+cityId);
+                db.close();
+            }
+        });
     }
 }
